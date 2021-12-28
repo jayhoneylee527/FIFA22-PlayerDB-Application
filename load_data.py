411154ee6a3d3278e1bc28ee3d0c55d997851660
Jae Hun Lee <55777012+jayhoneylee527@users.noreply.github.com>
2021-12-28 00:38:11 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -9,9 +9,5 @@ def load_players(file_name):
 	# Remove any columns with url
 	url_c = [c for c in df.columns if "url" in c]
 	df.drop(url_c, axis=1, inplace=True)
-	
-	# Convert dtypes of value and wage into int64 
-	df.fillna(0, inplace=True)
-	df = df.astype({'value_eur': 'int64', 'wage_eur': 'int64'})
 
 	return df
